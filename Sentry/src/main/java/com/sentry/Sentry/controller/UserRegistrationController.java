2bc057db3ab1c103bbd1980e4a2119acd66501bb
shr08 <shr08@mail.aub.edu>
2021-05-24 20:55:26 +0300

    updated signup

--- Sentry/src/main/java/com/sentry/Sentry/controller/UserRegistrationController.java
+++ Sentry/src/main/java/com/sentry/Sentry/controller/UserRegistrationController.java
@@ -26,7 +26,7 @@ public class UserRegistrationController {
     @GetMapping
     public String showRegistrationForm(Model model){
         model.addAttribute("user", new UserRegistrationDto());
-        return "testreg";
+        return "signup";
     }
 
     @PostMapping
